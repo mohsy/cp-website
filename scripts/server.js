de2a8ee376af7ca02a95d3ee755a83806027eb03
mohsy <52632506+mohsy@users.noreply.github.com>
2020-07-06 04:00:02 +0100

    Update server.js

--- scripts/server.js
+++ scripts/server.js
@@ -6,6 +6,7 @@ const express = require('express');
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+
 //Constatnts from original start.js 
 const concurrently = require('concurrently');
 const upath = require('upath');
@@ -13,10 +14,14 @@ const upath = require('upath');
 const browserSyncPath = upath.resolve(upath.dirname(__filename), '../node_modules/.bin/browser-sync');
 
 // App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+//const app = express();
+//app.get('/', (req, res) => {
+  //res.send('Hello World');
+//});
+
+app.use(express.static(path.join(__dirname, "dests/html.index", "public")
+
+//Serve website
 
 // From Original start.js script 
 
