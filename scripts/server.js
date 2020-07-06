9d76317a0ced5456173476dbc0ff06b0152852f2
mohsy <52632506+mohsy@users.noreply.github.com>
2020-07-06 02:56:13 +0100

    Update server.js

--- scripts/server.js
+++ scripts/server.js
@@ -14,31 +14,11 @@ const browserSyncPath = upath.resolve(upath.dirname(__filename), '../node_module
 
 // App
 const app = express();
-//app.get('/', (req, res) => {
-//  res.send('Hello World');
-//});
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 
 // From Original start.js script 
-concurrently([
-    { command: 'node scripts/sb-watch.js', name: 'SB_WATCH', prefixColor: 'bgBlue.bold' },
-    {
-        command: `"${browserSyncPath}" --reload-delay 2000 --reload-debounce 2000 dist -w --no-online`,
-        name: 'SB_BROWSER_SYNC',
-        prefixColor: 'bgGreen.bold',
-    }
-], {
-    prefix: 'name',
-    killOthers: ['failure', 'success'],
-}).then(success, failure);
-
-function success() {
-    console.log('Success');
-}
-
-function failure() {
-    console.log('Failure');
-}
-
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
