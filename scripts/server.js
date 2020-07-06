177057e233e63d9af3785f6b79cd9086a2598b1b
mohsy <52632506+mohsy@users.noreply.github.com>
2020-07-06 05:02:26 +0100

    Update server.js

--- scripts/server.js
+++ scripts/server.js
@@ -25,6 +25,13 @@ app.use(express.static(path.join(__dirname, "dist/index.html", "public")));
 //app.use("dist/html.index", express.static(path.join(__dirname, "public")))
 
 
+//Client side routing fix on page refresh or direct browsing to non-root directory
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..",  "public", "index.html"), err => {
+    if (err) {
+      res.status(500).send(err);
+    }
+
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
